c488856048945154c9ac7b0fee1aac3bdbb83253
David Ongaro <david.ongaro@hamburg.de>
2017-12-12 19:26:10 -0800

    Day 12 solution

---
+++ src/aoc2017/day12.clj
@@ -0,0 +1,31 @@
+(ns aoc2017.day12)
+
+(defn parse-connections []
+  (->> *in*
+       java.io.BufferedReader.
+       line-seq
+       doall
+       (map (fn [line] (clojure.string/split line #" <-> ")))
+       (reduce (fn [connections [node pipe-str]]
+                 (let [pipes (map #(Integer/valueOf %) (clojure.string/split pipe-str #", *"))]
+                   (assoc connections (Integer/valueOf node) pipes)))
+               {})))
+
+(defn transitive-closure [group new-members connections]
+  (let [new-members        
+        (clojure.set/difference (set (flatten (map connections new-members))) group)]
+    (if (empty? new-members)
+      group
+      (recur (clojure.set/union group new-members) new-members connections))))
+
+(defn count-group-size [rep]
+  (count (transitive-closure #{} #{rep} (parse-connections))))
+
+(defn count-groups []
+  (let [connections (parse-connections)]
+    (loop [rest (set (keys connections))
+           count 0]
+      (if (empty? rest)
+        count
+        (recur (clojure.set/difference rest (transitive-closure #{} #{(first rest)} connections))
+               (inc count))))))
---
+++ test/aoc2017/test12_test.clj
@@ -0,0 +1,33 @@
+(ns aoc2017.day12-test
+  (:use aoc2017.test-helper aoc2017.day12)
+  (:require [clojure.test :refer :all]))
+
+(deftest test-day12-part1
+  (testing "example"
+    (is (= (with-in-str "0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5" (count-group-size 0)) 6)))
+  (testing "puzzle input"
+    (binding [*in* (resource-as-stdin "day12-input.txt")]
+      (is (= (count-group-size 0)) 169))))
+
+(deftest test-day12-part1
+  (testing "example"
+    (is (= (with-in-str "0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5" (count-groups)) 2))
+    )
+  (testing "puzzle input"
+    (binding [*in* (resource-as-stdin "day12-input.txt")]
+      (is (= (count-groups) 179)))))
+
+
+
